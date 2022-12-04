28a1ff7984369611c14f8d8e674d1185f87020c5
tejasagashe <tejasagashe1999@gmail.com>
2022-12-04 20:28:52 +0530

    solution

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function bgclr(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var s = document.getElementById("sun");
+    s.classList.toggle("sun-circle");
+}
